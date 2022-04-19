bc0fa89df4ca93fa41dc514b8db244b8ab58bbf2
lclaudiomattoss <lclaudiomattoss@gmail.com>
2022-04-18 23:20:47 -0300

    Criacao do projeto
    criacao do projeto do app de fotos

---
+++ AppPhotos/AppPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppPhotos
+//
+//  Created by Luiz Claudio Mattos da Silva on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
